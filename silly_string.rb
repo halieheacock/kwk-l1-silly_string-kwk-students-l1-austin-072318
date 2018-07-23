4ea284e0af8680f72893e3a2ea7bbe3add7a0c44
halieheacock <haliebug1108@gmail.com>
2018-07-23 20:57:38 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,7 +1,7 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word  ""
+your_favorite_word = "I_Love_Chicken_Nuggets"
 
 puts "My favorite word is #{your_favorite_word}"
 
